e432ff323002075f9e5dc5071528905463bf1528
renzhsh <renzhsh@126.com>
2021-10-01 10:33:01 +0800

    引入Druid和JdbcTemplate

--- migration-code-first/src/main/java/com/liangma/migration/MigrationContext.java
+++ migration-code-first/src/main/java/com/liangma/migration/MigrationContext.java
@@ -3,9 +3,13 @@ package com.liangma.migration;
 import com.liangma.migration.annotations.Table;
 import com.liangma.migration.loaders.IAnnotatedClassLoader;
 import com.liangma.migration.logs.ILogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.lang.annotation.Annotation;
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -19,6 +23,14 @@ public class MigrationContext {
 
     private final ILogger log;
 
+    //数据源组件
+    @Autowired
+    private DataSource dataSource;
+
+    //用于访问数据库的组件
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     /**
      * @param annotatedClassLoader
      * @param log
@@ -29,10 +41,16 @@ public class MigrationContext {
     }
 
     public void execute() {
-        List<Class<?>> list = annotatedClassLoader.getTypesAnnotatedWith(Table.class);
 
-        for (Class<?> item : list) {
-            System.out.println(item);
+        try {
+            System.out.println("默认数据源为：" + dataSource.getClass());
+            System.out.println("数据库连接实例：" + dataSource.getConnection());
+            //访问数据库
+            Integer i = jdbcTemplate.queryForObject("SELECT count(*) from `clients`", Integer.class);
+            System.out.println("user 表中共有" + i + "条数据。");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
         }
     }
 }
--- migration-code-first/src/main/java/com/liangma/migration/config/MigrationConfig.java
+++ migration-code-first/src/main/java/com/liangma/migration/config/MigrationConfig.java
@@ -1,16 +1,30 @@
 package com.liangma.migration.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.liangma.migration.MigrationContext;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
-@ConfigurationProperties(prefix = "migration")
 public class MigrationConfig {
 
     @Bean
     public ClassLoader classLoader() {
         return this.getClass().getClassLoader();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public JdbcTemplate jdbdTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
